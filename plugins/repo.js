3c52021db2a10f5fa7a6cfa3b447248d3b539d38
salvator509 <alphaboymd509@gmail.com>
2025-06-07 20:15:33 -0400

    Create repo.js

---
+++ plugins/repo.js
@@ -0,0 +1,72 @@
+const fetch = require('node-fetch');
+const config = require('../config');
+const { cmd } = require('../command');
+
+cmd({
+    pattern: "repo",
+    alias: ["sc", "script", "info"],
+    desc: "Fetch GitHub repository information",
+    react: "📂",
+    category: "info",
+    filename: __filename,
+},
+async (conn, mek, m, { from, reply }) => {
+    const githubRepoURL = 'https://github.com/salvtore509 /alphaboy-tech-md-v1;
+
+    try {
+        const match = githubRepoURL.match(/github\.com\/([^/]+)\/([^/]+)/);
+        if (!match) return reply("❌ Erreur : L'URL du repo est invalide.");
+
+        const [, username, repoName] = match;
+
+        const response = await fetch(`https://api.github.com/repos/${username}/${repoName}`, {
+            headers: {
+                'User-Agent': 'GOTAR-XMD'
+            }
+        });
+
+        if (response.status === 503) {
+            return reply("❌ GitHub est temporairement indisponible (503). Réessaie plus tard.");
+        }
+
+        if (!response.ok) {
+            return reply(`❌ Échec de récupération des infos du repo. Code: ${response.status}`);
+        }
+
+        const repoData = await response.json();
+
+        const message = `┌──────────────────────┐
+│  😴 ALPHABOY-TECH-MD-V1 𝗥𝗘𝗣𝗢  😴 
+├──────────────────────
+│ • Name: ${repoData.name}
+│ • Owner: ${repoData.owner.login}
+│ • Stars: ${repoData.stargazers_count}
+│ • Forks: ${repoData.forks_count}
+│ • URL: ${repoData.html_url}
+│ • Desc: ${repoData.description || 'None'}
+└──────────────────────┘
+> *MADE IN PRINCE SALVATORE🇭🇹*`;
+
+        await conn.sendMessage(from, {
+            image: { url: `https://files.catbox.moe/egrm42.jpg ` },
+            caption: message,
+            contextInfo: { 
+                mentionedJid: [m.sender],
+                forwardingScore: 999,
+                isForwarded: true,
+                forwardedNewsletterMessageInfo: {
+                  
+                    newsletterName: config.OWNER_NAME || 'PRINCE SALVATORE,
+                    serverMessageId: 143
+                }
+            }
+        }, { quoted: mek });
+
+    } catch (error) {
+        console.error("Repo command error:", error);
+        reply("❌ Une erreur est survenue lors de la récupération du dépôt.");
+    }
+});
+     reply("❌ Une erreur est survenue lors de la récupération du dépôt.");
+    }
+});
